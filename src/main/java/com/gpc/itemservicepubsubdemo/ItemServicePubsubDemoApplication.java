5acfd0dd413c7fbf48670769067dff73b45a56de
Harish K <hckukatla@gmail.com>
2022-09-22 00:53:02 -0400

    initial version

---
+++ src/main/java/com/gpc/itemservicepubsubdemo/ItemServicePubsubDemoApplication.java
@@ -0,0 +1,13 @@
+package com.gpc.itemservicepubsubdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemServicePubsubDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemServicePubsubDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gpc/itemservicepubsubdemo/ItemServicePubsubDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gpc.itemservicepubsubdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemServicePubsubDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
